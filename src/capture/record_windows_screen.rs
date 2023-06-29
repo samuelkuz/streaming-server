b007545338fe45a3b6347fe2907c478ca393b829
Samuel Kuznia <samuelkuz@gmail.com>
2023-06-29 15:02:46 -0500

    Add initial hacky WebRTC integration

--- src/capture/record_windows_screen.rs
+++ src/capture/record_windows_screen.rs
@@ -11,19 +11,25 @@ use futures_util::{SinkExt, StreamExt};
 use tokio::net::{TcpStream};
 use tokio_tungstenite::{WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
-
-
+// WebRTC imports
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use webrtc::media::Sample;
+use webrtc::track::track_local::track_local_static_sample::TrackLocalStaticSample;
+use bytes::Bytes;
 
 pub async fn record(
         mut windows_screen_capture: WindowsScreenCapture, 
         mut encoder: FfmpegEncoder,
-        mut ws_stream: WebSocketStream<TcpStream>,
+        mut video_track: Arc<TrackLocalStaticSample>,
     ) {
     let mut receiver = windows_screen_capture.get_frame_receiver().unwrap();
     windows_screen_capture.start_capture_session();
 
+    let FRAME_RATE = 30;
+
     let mut ticker =
-        tokio::time::interval(Duration::from_millis((1000 / 30) as u64));
+        tokio::time::interval(Duration::from_millis((1000 / FRAME_RATE) as u64));
     
     let test_frames = 1200;
     let mut frame_idx: i64 = 0;
@@ -39,7 +45,21 @@ pub async fn record(
         let encoded = encoder.encode(frame_bits, frame_time).unwrap();
         //write(&mut file, &encoded).await.unwrap();
         
-        ws_stream.send(Message::binary(encoded)).await.unwrap();
+        // self.video_track
+        //     .write_sample(&Sample {
+        //         data: input,
+        //         duration: Duration::from_millis((1000. / self.frame_rate as f64) as u64),
+        //         ..Default::default()
+        //     })
+        //     .await
+        //     .expect("TODO: panic message");
+
+        let bytes = Bytes::from(encoded);
+        video_track.write_sample(&Sample {
+            data: bytes,
+            duration: Duration::from_millis((1000. / FRAME_RATE as f64) as u64),
+            ..Default::default()
+        }).await.expect("Could not write sample");
 
         unsafe {
             windows_screen_capture.unmap_d3d_context(&resource);
---
+++ src/signal_message.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum SignalRequest {
+    Offer {
+        id: String,
+        sdp: String,
+    },
+    Answer {
+        id: String,
+        sdp: String,
+    },
+    CallerIceCandidate {
+        id: String,
+        ice_candidate: String,
+    },
+    GetOffer {
+        id: String,
+    },
+    PeerIceCandidate {
+        id: String,
+        ice_candidate: String,
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Test {
+    pub sdp: String,
+    pub r#type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum SignalResponse {
+    Offer {
+        sdp: String,
+    },
+    Answer {
+        sdp: String,
+    },
+    IceCandidate {
+        ice_candidate: String,
+    },
+}
+
